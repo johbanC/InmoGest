48740a1289ec5c2e489e27986240eb689a260839
Johban Clavijo <johbancgmail.com>
2023-12-19 17:19:49 -0500

    Creacion del home V1

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,40 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
+Router::get Consultar
+Router::post Guardar
+Router::delete Eliminar
+Router::put Actualizar
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return "ruta home";
+    //return view('welcome');
+    return view('home'); //esta vista no existe
+});
+
+
+Route::get('blog', function () {
+    //consulta en la base de datos
+    $posts = [
+        ['id' => 1, 'title' => 'PHP', 'slug' => 'php'],
+        ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel']
+    ];
+
+
+    return view('blog', ['$posts' => $posts]);
+});
+
+Route::get('blog/{slug}', function ($slug) {
+    //consulta
+    $post =  $slug;
+
+    return view('post', ['$post' => $post]);
+});
+
+Route::get('buscar', function (Request $request) {
+    return $request->all();
 });
