2fd9d6f0d81813dc63cce3574469c484420c041c
Johban Clavijo <johbanc@gmail.com>
2024-05-20 00:21:01 -0500

    Primer Commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                © {{ date('Y') }} InmoGest<span class="d-none d-sm-inline-block"> - Fabricado con <i class="mdi mdi-heart text-danger"></i> por EnDigitalWeb.</span>
+            </div>
+        </div>
+    </div>
+</footer>
