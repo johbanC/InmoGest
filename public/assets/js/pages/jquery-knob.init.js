f6e9305507679512110ec6fa1be9e45a70bcd084
Johban Clavijo <johbanc@gmail.com>
2024-06-19 00:40:37 -0500

    Actualizacion de de pendencias para lanzamiento

--- public/assets/js/pages/jquery-knob.init.js
+++ public/assets/js/pages/jquery-knob.init.js
@@ -1,18 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!************************************************!*\
-  !*** ./resources/js/pages/jquery-knob.init.js ***!
-  \************************************************/
-/*
-Template Name: Veltrix - Admin & Dashboard Template
-Author: Themesbrand
-Website: https://themesbrand.com/
-Contact: themesbrand@gmail.com
-File: Jquery knob init Js File
-*/
-
-$(function () {
-  $(".knob").knob();
-});
-/******/ })()
-;
+$((function(){$(".knob").knob()}));
