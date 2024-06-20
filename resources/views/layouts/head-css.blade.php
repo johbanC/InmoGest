2bff820f992ea72751f805b006f7041318b47b39
Johban Clavijo <johbanc@gmail.com>
2024-06-19 23:06:59 -0500

    Cambio de laravel mix a laravel vite

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -6,3 +6,5 @@
 <link href="{{URL::asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css">
 <!-- App Css-->
 <link href="{{URL::asset('assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css">
+
+@vite(['resources/css/app.css', 'resources/js/app.js'])
