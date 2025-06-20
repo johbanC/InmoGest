4a7cba6cbb5a53460643e3493380ddbec7594105
Johban Clavijo <johbanc@gmail.com>
2025-06-20 15:11:54 -0500

    cambio de nombre de clase del seeder

--- database/seeders/TipoClienteSeeder.php
+++ database/seeders/TipoClienteSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class TipoClientesSeeder extends Seeder
+class TipoClienteSeeder extends Seeder
 {
     public function run()
     {
