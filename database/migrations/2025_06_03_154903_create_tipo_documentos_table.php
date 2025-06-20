e06a0058daf73f073c651cd76c9d71838ac00939
Johban Clavijo <johbanc@gmail.com>
2025-06-20 14:25:31 -0500

    Migracion actualizada

--- database/migrations/2025_06_03_154903_create_tipo_documentos_table.php
+++ database/migrations/2025_06_03_154903_create_tipo_documentos_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('acronimo')->nullable();
             $table->string('descripcion')->nullable();
             $table->foreignId('tipo_estatus_id')->constrained('tipo_estatuses');
+            $table->foreignId('tipo_estatus_id')->constrained('tipo_estatuses');
             $table->foreignId('user_id')->constrained('users');
                 
 
