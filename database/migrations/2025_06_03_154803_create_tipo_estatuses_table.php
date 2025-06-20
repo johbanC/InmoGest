40123098e480f238037ebed2d0af046ee2e144cd
Johban Clavijo <johbanc@gmail.com>
2025-06-20 14:31:18 -0500

    actualizacion de migracion

--- database/migrations/2025_06_03_154803_create_tipo_estatuses_table.php
+++ database/migrations/2025_06_03_154803_create_tipo_estatuses_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('tipo_documentos', function (Blueprint $table) {
+        Schema::create('tipo_estatuses', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('nombre');
